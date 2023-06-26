d47340a0c8fe89b73768de5d5ce146be84d6a316
Maxwell11 <maksikbestbest@gmail.com>
2023-06-26 14:29:50 +0300

    dz6 done !

---
+++ number41/Program.cs
@@ -0,0 +1,19 @@
+﻿void InOutputArray(){
+    int a = 0 ;
+    Console.Write("Введите ваши числа : "); 
+    int[] result = Array.ConvertAll(Console.ReadLine()!.Split() , int.Parse );
+    for (int i = 0 ; i < result.Length ; i++ ){
+        if (result[i] > 0){
+            a = a + 1;
+        }
+    }
+    Console.Write("Ваши числа: ");
+    for (int i = 0 ; i < result.Length ; i++ ){
+        Console.Write(result[i]);
+        if ( i != (result.Length-1)){
+            Console.Write(", ");
+        }
+    }
+    Console.Write(" -> чисел больше 0 : {0} ", a);
+}
+InOutputArray();
---
+++ number43/Program.cs
@@ -0,0 +1,15 @@
+﻿double ReadNumber(string cord){
+    Console.Write("Введите {0}: " , cord);
+    return (Convert.ToDouble(Console.ReadLine())); 
+}
+void distance(){
+    double k1 = ReadNumber("k1");
+    double b1 = ReadNumber("b1");
+    double k2 = ReadNumber("k2");
+    double b2 = ReadNumber("b2");
+    double x = ((b1-b2)/(k2-k1));
+    double y = k1*x + b1 ;
+    Console.WriteLine("Координаты пересечения : ({0};{1}) ", Math.Round(x,3) , Math.Round(y,3) ); 
+}
+distance();
+//y = k1 * x + b1, y = k2 * x + b2; 
